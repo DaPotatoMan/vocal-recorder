fc3b09ccd067c09bffd0667c8c1754b73ad0dcfd
Fahadul Islam <44726129+DaPotatoMan@users.noreply.github.com>
2022-12-25 21:56:04 +0600

    feat: Update build scripts

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,9 +1,25 @@
 import { resolve } from 'path'
+import { writeFileSync } from 'fs'
+
 import { defineConfig } from 'vitest/config'
 import dts from 'vite-plugin-dts'
 
+import { version } from '../../package.json'
+import manifest from './public/package.json'
+
 export default defineConfig({
-  plugins: [dts({ insertTypesEntry: true })] as any,
+  plugins: [
+    dts({ insertTypesEntry: true }) as any,
+
+    // Update package.json version
+    {
+      buildStart() {
+        const pkgPath = 'public/package.json'
+        manifest.version = version
+        writeFileSync(pkgPath, JSON.stringify(manifest, null, 2))
+      }
+    }
+  ],
 
   build: {
     lib: {
