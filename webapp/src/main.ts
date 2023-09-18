63c726c7af95f26e0755b4cefd068f1fe7f2d50b
dapotatoman <iam.fahadul@gmail.com>
2023-09-18 20:49:20 +0600

    cleanup: webapp

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -3,7 +3,7 @@ import 'uno.css'
 import './style.css'
 
 import { createPinia } from 'pinia'
-import App from './NewApp.vue'
+import App from './App.vue'
 
 createApp(App)
   .use(createPinia())
--- webapp/src/store.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia'
-import { markRaw } from 'vue'
-import { Recorder } from '../../packages/vocal-recorder/src'
-
-export const useRecorder = defineStore('recorder', {
-  state: () => ({
-    recorder: markRaw(new Recorder())
-  })
-})
