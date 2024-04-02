7b9bae95b92406419f1c53cebb05fc8fbb024c57
dapotatoman <iam.fahadul@gmail.com>
2024-04-02 23:41:15 +0600

    chore: update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  isInEditor: false,
+
+  stylistic: {
+    indent: 2,
+    quotes: 'single'
+  },
+
+  rules: {
+    'no-console': 'off',
+    'style/comma-dangle': ['error', 'never']
+  }
+})
