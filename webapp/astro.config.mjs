7081bdfc1d918c1024b4bb1e7415a760b7ffe9e4
Fahad <44726129+DaPotatoMan@users.noreply.github.com>
2024-12-13 17:23:59 +0600

    feat(webapp): update

--- webapp/astro.config.mjs
+++ webapp/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  base: process.env.CI ? '/vocal-recorder': undefined,
   integrations: [
     unocss({ injectReset: true }),
     solid({ devtools: true })
