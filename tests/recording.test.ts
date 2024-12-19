d4d0aceb65d6ead00611befb3cc24a895dec88de
Fahad <44726129+DaPotatoMan@users.noreply.github.com>
2024-12-19 15:11:19 +0600

    chore: update tests

--- tests/recording.test.ts
+++ tests/recording.test.ts
@@ -1,11 +1,8 @@
 import { sleep } from '@antfu/utils'
-import { AudioRecorder } from '../../src'
+import { AudioRecorder } from '../src'
 
 describe('class: AudioRecorder', () => {
   const recorder = new AudioRecorder()
-
-  const emitter = vi.fn()
-  recorder.events.on('*', emitter)
 
   it('can record', async () => {
     await recorder.init()
@@ -33,10 +30,6 @@ describe('class: AudioRecorder', () => {
     expect(result.peaks.length).not.toBe(0)
     expect(result.duration).greaterThanOrEqual(4500)
   }, 10_000)
-
-  it('emits proper events', () => {
-    // expect(emitter).toMatchSnapshot()
-  })
 
   it('disposes properly', () => {
     expect(recorder.stream?.active).toBe(false)
